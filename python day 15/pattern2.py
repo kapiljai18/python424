cf1474a83e9cea5469a58869ad0ae3faae2dc3df
kapiljai18 <kapiljaitapkar2004@gmail.com>
2026-03-10 09:01:39 +0000

    kapsssyyy

---
+++ python day 15/pattern2.py
@@ -0,0 +1,58 @@
+# # left increment triange
+# count=int(input("enter row count: "))
+# for i in range(count):
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+
+
+
+# # left decrement triange
+# count=int(input("enter row count: "))
+# for i in range(count):
+#     for j in range(count,i,-1):
+#         print("*",end="")
+#     print()
+
+
+# # right increment triangle
+# count=int(input("enter row count: "))
+# for i in range(count):
+#     for k in range(count,i+1,-1):
+#         print(end=" ")
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+
+
+# # right decrement triange
+# count=int(input("enter row count: "))
+# for i in range(count):
+#     for k in range(i):
+#         print(end=" ")
+#     for j in range(count,i,-1):
+#         print("*",end="")
+#     print()
+
+
+# # hill-increment triange
+# count=int(input("enter row count: "))
+# for i in range(count):
+#     for k in range(count,i+1,-1):
+#         print(end=" ")
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+
+# # hill-decrement triange
+# count=int(input("enter row count: "))
+# for i in range(count):
+#     for k in range(i):
+#         print(end=" ")
+#     for j in range(count,i,-1):
+#         print("*",end=" ")
+#     print()
+
+
+
